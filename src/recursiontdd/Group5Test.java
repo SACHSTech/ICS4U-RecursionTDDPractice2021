3278877afb77a406805479f73512aec3dc67d4cf
Tiffany Lee <>
2021-05-12 17:30:39 +0000

    test file

---
+++ src/recursiontdd/Group5Test.java
@@ -0,0 +1,23 @@
+package recursiontdd;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import recursiontdd.Group5; //rename to reference proper class
+
+/**
+* See https://github.com/mrfabroa/Fabroa-ICS4U-NotesExamples/blob/main/src/tdd/TDDExampleTest.java for an example
+*/
+
+public class Group5Test { // rename class to refere proper class
+
+    /**
+     * An initial test
+     */
+    @Test
+    public void Test1(){
+        // make assertion statement(s)
+    }
+
+   //Add additional tests
+  
+}
